a99378ede503275aea3dece36db25e8a0ef2d45f
Cece Zinny <cwzinny@gmail.com>
2018-07-22 22:56:20 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,46 +1,58 @@
-STUDENT_NAMES = [
-  "Adele",
-  "Beyoncé",
-  "Cardi B",
-  "Lady Gaga",
-  "Nicki Minaj",
-  "Rihanna"
-]
 
 
 
-# puts STUDENT_NAMES[0]
-
-# puts STUDENT_NAMES[3]
-
-# puts STUDENT_NAMES[5]
 
 
-def first_student_by_index
-   STUDENT_NAMES[0]
-end
-
-# puts first_student_by_index
 
 
-def fourth_student_by_index
-  puts STUDENT_NAMES[3]
-end 
 
 
-def last_student_by_method
-  STUDENT_NAMES[5]
-end
-
-# puts last_student_by_method
-
-def first_second_and_third_students
- puts  STUDENT_NAMES[0..2]
-# shows that it is between 0 and 2, don't need the 1
-end
-
-# puts first_second_and_third_students
 
 
-# puts fourth_student_by_index
+
+# STUDENT_NAMES = [
+#   "Adele",
+#   "Beyoncé",
+#   "Cardi B",
+#   "Lady Gaga",
+#   "Nicki Minaj",
+#   "Rihanna"
+# ]
+
+
+
+# # puts STUDENT_NAMES[0]
+
+# # puts STUDENT_NAMES[3]
+
+# # puts STUDENT_NAMES[5]
+
+
+# def first_student_by_index
+#   STUDENT_NAMES[0]
+# end
+
+# # puts first_student_by_index
+
+
+# def fourth_student_by_index
+#   puts STUDENT_NAMES[3]
+# end 
+
+
+# def last_student_by_method
+#   STUDENT_NAMES[5]
+# end
+
+# # puts last_student_by_method
+
+# def first_second_and_third_students
+# puts  STUDENT_NAMES[0..2]
+# # shows that it is between 0 and 2, don't need the 1
+# end
+
+# # puts first_second_and_third_students
+
+
+# # puts fourth_student_by_index
 
